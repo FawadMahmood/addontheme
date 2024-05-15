78aec8ffbd5038c32d2b2cd26eaf7932e23bf16c
Fawad Mahmood Siddiqui <fawad@washmen.com>
2024-05-15 18:38:51 +0500

    chore: clean up

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -52,58 +52,6 @@ const preview: Preview = {
   },
 };
 
-
-// const themes = {
-//   default: {
-//     // Define your default theme properties
-//     backgroundColor: '#007bff',
-//     hoverColor: '#0056b3',
-//     activeColor: '#004080',
-//     textColor: '#fff',
-//   },
-//   red: {
-//     // Define your red theme properties
-//     backgroundColor: '#ff0000',
-//     hoverColor: '#cc0000',
-//     activeColor: '#990000',
-//     textColor: '#fff',
-//   },
-//   green: {
-//     // Define your green theme properties
-//     backgroundColor: '#00cc00',
-//     hoverColor: '#009900',
-//     activeColor: '#006600',
-//     textColor: '#fff',
-//   },
-//   blue: {
-//     // Define your blue theme properties
-//     backgroundColor: '#0000ff',
-//     hoverColor: '#0000cc',
-//     activeColor: '#000099',
-//     textColor: '#fff',
-//   },
-//   purple: {
-//     // Define your purple theme properties
-//     backgroundColor: '#800080',
-//     hoverColor: '#660066',
-//     activeColor: '#4c004c',
-//     textColor: '#fff',
-//   },
-//   yellow: {
-//     // Define your yellow theme properties
-//     backgroundColor: '#ffff00',
-//     hoverColor: '#cccc00',
-//     activeColor: '#999900',
-//     textColor: '#fff',
-//   },
-//   orange: {
-//     // Define your orange theme properties
-//     backgroundColor: '#ff8000',
-//     hoverColor: '#cc6600',
-//     activeColor: '#994d00',
-//     textColor: '#fff',
-//   },
-// };
 export const globalTypes = {
   theme:{
     type:'string',
@@ -114,8 +62,6 @@ export const globalTypes = {
     }
   },
 }
-
-
 
 export default preview;
 
